ae6202a4686eaa6a0701084e57dc5763da14acc8
balajivishwa <balajivishwa1712@gmail.com>
2021-06-14 11:48:26 +0530

    donig

--- reverse_num.c
+++ reverse_num.c
@@ -1,11 +1,25 @@
 #include<stdio.h>
 #include<math.h>
-int recerse(int a ){
 
+
+int recerse(int a ){
+int reminder,rev=0;
+while (a!=0)
+{
+    reminder=a%10;
+    rev=rev*10+reminder;
+    a=a/10;
+}
+printf("the reversed number is : %d",rev);
 
 
 
 }
 int main(){
+    int a;
+    printf("enter a number for reversing : ");
+    scanf("%d",&a);
+    recerse(a);
+
 
 }
